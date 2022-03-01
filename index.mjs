82e26eca085d9d5bc566b953dfa1e13f87ffbed6
Alexander Gabriel <alex.barbalex@gmail.com>
2022-03-01 16:09:21 +0100

    extract events

--- index.mjs
+++ index.mjs
@@ -15,5 +15,26 @@ const pgClient = new Client({
 })
 await pgClient.connect()
 
-const events = await bb.list({ type: 'events' })
+// 1. import event
+const rawEvents = await bb.list({ type: 'events', include_docs: true })
+const events = rawEvents.rows.map((e) => {
+  const dat = e.id.substr(7, 17)
+  return {
+    id: e.id,
+    datum: `${dat.substr(0, 4)}-${dat.substr(5, 2)}-${dat.substr(8, 2)}`,
+    title: e.doc.title,
+    links: e.doc.links,
+    event_type: e.doc.eventType,
+    tags: e.doc.tags,
+  }
+})
+//console.log('rawEvents:', rawEvents)
 console.log('events:', events)
+
+// 2. import article
+
+// 3. import monthly_event
+
+// 4. import page
+
+// 4. import publication
