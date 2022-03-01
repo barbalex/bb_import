a22405f3747421999763f245820f7eaae0be81aa
Alexander Gabriel <alex.barbalex@gmail.com>
2022-03-01 18:25:42 +0100

    import pages

--- index.mjs
+++ index.mjs
@@ -21,60 +21,94 @@ const rows = result.rows
   .map((r) => r.doc)
   .filter((e) => !e._id.includes('Invalid date'))
 
-// 1. import event
-const events = rows
-  .filter((r) => r.type === 'events')
+// // 1. import event
+// const events = rows
+//   .filter((r) => r.type === 'events')
+//   .map((e) => {
+//     // events_
+//     const dat = e._id.substr(7, 17)
+
+//     return {
+//       datum: `${dat.substr(0, 4)}-${dat.substr(5, 2)}-${dat.substr(8, 2)}`,
+//       title: e.title,
+//       links: e.links.filter((l) => !!l.url),
+//       event_type: e.eventType,
+//       tags: e.tags,
+//     }
+//   })
+// //console.log('events:', events)
+
+// events.forEach((e) => {
+//   pgClient.query(
+//     `insert into event(datum, title, links, event_type, tags) values($1, $2, $3, $4, $5)`,
+//     [
+//       e.datum,
+//       e.title,
+//       e.links?.length ? JSON.stringify(e.links) : null,
+//       e.event_type,
+//       e.tags?.length ? JSON.stringify(e.tags) : null,
+//     ],
+//   )
+// })
+
+// // 2. import article
+// const articles = rows
+//   .filter((r) => r.type === 'articles')
+//   .map((e) => {
+//     // commentaries_
+//     const dat = e._id.substr(13, 23)
+
+//     return {
+//       datum: `${dat.substr(0, 4)}-${dat.substr(5, 2)}-${dat.substr(8, 2)}`,
+//       title: e.title,
+//       content: e.article,
+//     }
+//   })
+// //console.log('articles:', articles)
+
+// articles.forEach((e) => {
+//   pgClient.query(
+//     `insert into article(datum, title, content) values($1, $2, $3)`,
+//     [e.datum, e.title, e.content],
+//   )
+// })
+
+// // 3. import monthly_event
+// const monthlyEvents = rows
+//   .filter((r) => r.type === 'monthlyEvents')
+//   .map((e) => {
+//     // monthlyEvents_
+//     const dat = e._id.substr(14, 24)
+
+//     return {
+//       datum: `${dat.substr(0, 4)}-${dat.substr(5, 2)}-28`,
+//       content: e.article,
+//     }
+//   })
+// //console.log('monthlyEvents:', monthlyEvents)
+
+// monthlyEvents.forEach((e) => {
+//   pgClient.query(`insert into monthly_event(datum, content) values($1, $2)`, [
+//     e.datum,
+//     e.content,
+//   ])
+// })
+
+// 4. import page
+const pages = rows
+  .filter((r) => r.type === 'pages')
   .map((e) => {
-    // events_
-    const dat = e._id.substr(7, 17)
-
     return {
-      datum: `${dat.substr(0, 4)}-${dat.substr(5, 2)}-${dat.substr(8, 2)}`,
-      title: e.title,
-      links: e.links.filter((l) => !!l.url),
-      event_type: e.eventType,
-      tags: e.tags,
-    }
-  })
-//console.log('events:', events)
-
-events.forEach((e) => {
-  pgClient.query(
-    `insert into event(datum, title, links, event_type, tags) values($1, $2, $3, $4, $5)`,
-    [
-      e.datum,
-      e.title,
-      e.links?.length ? JSON.stringify(e.links) : null,
-      e.event_type,
-      e.tags?.length ? JSON.stringify(e.tags) : null,
-    ],
-  )
-})
-
-// 2. import article
-const articles = rows
-  .filter((r) => r.type === 'articles')
-  .map((e) => {
-    // commentaries_
-    const dat = e._id.substr(13, 23)
-
-    return {
-      datum: `${dat.substr(0, 4)}-${dat.substr(5, 2)}-${dat.substr(8, 2)}`,
-      title: e.title,
+      name: e._id.replace('pages_', ''),
       content: e.article,
     }
   })
-//console.log('articles:', articles)
 
-articles.forEach((e) => {
-  pgClient.query(
-    `insert into article(datum, title, content) values($1, $2, $3)`,
-    [e.datum, e.title, e.content],
-  )
+pages.forEach((e) => {
+  pgClient.query(`insert into page (name, content) values($1, $2)`, [
+    e.name,
+    e.content,
+  ])
 })
-
-// 3. import monthly_event
-
-// 4. import page
 
 // 4. import publication
