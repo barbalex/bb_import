57fbdf498ecf25e0a76f5cabb3e53d4a30ec7614
Alexander Gabriel <alex.barbalex@gmail.com>
2022-03-01 15:29:20 +0100

    fetch events

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const secrets = require('./secrets.json')
 
 const nano = require('nano')(secrets.conn_string)
 const bb = nano.use('bb')
-
-console.log('bb:', bb)
+const events = bb
+  .list({ type: 'events' })
+  .then((val) => console.log('event:', val))
